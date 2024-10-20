bb7901e7ca216dd9f119626a3b2c89963da08901
Fawaz <fawazming@gmail.com>
2024-10-20 10:19:57 +0100

    24 update

--- ci/app/Views/header.php
+++ ci/app/Views/header.php
@@ -7,7 +7,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel='shortcut icon' href='<?= base_url('assets/img/favicon.jpg') ?>'>
 	<meta name="robots" CONTENT="noindex, nofollow">
-	<title>PMC 2020:: Statistics Admin Panel</title><!-- GLOBAL VENDORS-->
+	<title>PMC:: Vendor Panel</title><!-- GLOBAL VENDORS-->
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" media="all">
 	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
 	<link href="<?= base_url('assets/admin/vendors/feather-icons/feather.css') ?>" rel="stylesheet" />
